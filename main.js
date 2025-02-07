65f82faa0687fef8328cbfa4033df6d4f6530576
terrywh <terry.wuhao@gmail.com>
2025-02-07 21:39:34 +0800

    进行一定程度的美化并保持对 Kodi 的兼容；

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import { serve } from "bun";
 import { readdir } from "node:fs/promises";
 import { basename } from "node:path";
-import { join } from "node:path/posix";
+import { extname, join } from "node:path/posix";
 
 /**
  * 
@@ -9,7 +9,9 @@ import { join } from "node:path/posix";
  * @returns {string}
  */
 function formatSize(size) {
-    if (size > 1.1 * 1024 * 1024 * 1024) {
+    if (size === null) {
+        return "-";
+    } else if (size > 1.1 * 1024 * 1024 * 1024) {
         return `${(size / 1024 / 1024 / 1024).toFixed(1)}G`;
     } else if (size > 1.1 * 1024 * 1024) {
         return `${(size / 1024 / 1024).toFixed(1)}M`;
@@ -24,9 +26,63 @@ function formatSize(size) {
  * @returns {string}
  */
 function formatDate(date) {
+    if (!date) return "-";
     return `${date.getFullYear().toString().padStart(4, '0')}-${(date.getMonth()+1).toString().padStart(2, '0')}-${date.getDate().toString().padStart(2, '0')} ${date.getHours().toString().padStart(2, '0')}:${date.getMinutes().toString().padStart(2, '0')}`;
 }
 
+/**
+ * 
+ * @param {Request} req 
+ * @param {import("bun").BunFile} file
+ * @returns {Array<number>}
+ */
+function parseRange(req, file) {
+    const range = req.headers.get("Range");
+    return range ? range.split("=").at(-1).split("-").map((v) => {
+        return v ? Number(v) : file.size;
+    }) : [0, file.size];
+}
+
+function fetchIcon(path) {
+    switch (extname(path)) {
+    case ".mp4":
+    case ".mkv":
+    case ".webm":
+        return `<i class="bi bi-film"></i>`;
+    case ".css":
+    case ".js":
+    case ".json":
+    case ".yaml":
+    case ".toml":
+    case ".html":
+    case ".php":
+    case ".cpp":
+    case ".go":
+    case ".rs":
+        return `<i class="bi bi-file-code"></i>`;
+    case ".flac":
+    case ".wma":
+    case ".mp3":
+    case ".wav":
+    case ".ogg":
+        return `<i class="bi bi-file-music"></i>`;
+    case ".gif":
+    case ".bmp":
+    case ".jpg":
+    case ".jpeg":
+    case ".heic":
+    case ".webp":
+        return `<i class="bi bi-file-image"></i>`;
+    case ".txt":
+    case ".md":
+        return `<i class="bi bi-file-text"></i>`;
+    default:
+        if (path == "../") return `<i class="bi bi-box-arrow-in-up"></i>`
+        else if (path.endsWith("/")) return `<i class="bi bi-folder"></i>`;
+        else return "";
+    };
+    
+}
 
 class SourceBuilder {
     /**
@@ -45,22 +101,42 @@ class SourceBuilder {
      * @param {*} size 
      */
     file(path, name, date, size) {
-        this.#html += `<tr>
-            <td><a href="${encodeURIComponent(path)}">${name}</a></td>
-            <td>${formatDate(date)}</td>
-            <td>${formatSize(size)}</td>
-        </tr>
-        `;
+        this.#html += `<tr>`;
+        this.#html += `<td>${fetchIcon(path)} <a href="${path}">${name}</a></td>`;
+        if (date) {
+            this.#html += `<td class="text-secondary"><i class="bi bi-calendar2-day"></i> ${formatDate(date)}</td>`;
+        } else {
+            this.#html += `<td> - </td>`;
+        }
+        this.#html += `<td class="text-secondary">${formatSize(size)}</td>`;
+        this.#html += `</tr>`;
+    }
+
+    async directory(path) {
+        if (path != "./") {
+            this.file("../", `上一层`, null, null);
+        }
+        const entries = await readdir(path);
+        for (const entry of entries) {
+            const filepath = join(path, entry)
+            const filelink = encodeURIComponent(entry);
+
+            const file = Bun.file(filepath);
+            const stat = await file.stat();
+            this.file(stat.isDirectory() ? `${filelink}/` : `${filelink}`, basename(entry), stat.mtime, stat.isDirectory() ? null : stat.size);
+        }
     }
 
     reset() {
-        this.#html = `<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+        this.#html = `
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" crossorigin="anonymous">
 <table class="table">
     <thead>
         <tr>
-            <td>名称</td>
-            <td>时间</td>
-            <td>大小</td>
+            <th>名称</th>
+            <th>时间</th>
+            <th>大小</th>
         </tr>
     </thead>
     <tbody>
@@ -77,53 +153,47 @@ class SourceBuilder {
 
 }
 
+
+
 serve({
     async fetch(req) {
         const url = new URL(req.url);
-        console.info(new Date(), req.method, url.pathname);
-        if (url.pathname == "/") {
-            const sb = new SourceBuilder();
-            const files = await readdir("./");
-            for (const file of files) {
-                const f = Bun.file(file);
-                const stats = await f.stat();
-                sb.file(file, basename(file), stats.ctime, stats.size);
-            }
-            return new Response(sb.done(), {
+        const path = url.pathname.split("/").map(decodeURIComponent).join("/");
+        const file = Bun.file(join("./", path));
+
+        console.info(new Date(), req.method, path);
+
+        let stat;
+        try {
+            stat = await file.stat();
+        } catch(ex) {
+            return new Response(null, {status: 404});
+        }
+        
+        if (stat.isDirectory()) {
+            const builder = new SourceBuilder();
+            await builder.directory(file.name);
+            return new Response(builder.done(), {
                 headers: {
                     "content-type": "text/html; charset=utf-8",
                 },
             });
+        } else if (req.method == "HEAD") {
+            return new Response(f.slice(0, 0));
+        } else if (extname(file.name) == "") {
+            return new Response(file, {
+                headers: {
+                    "content-type": "text/plain",
+                },
+            });
         } else {
-            const range = req.headers.get("Range")
-            const f = Bun.file(join("./", url.pathname));
-            const exists = await f.exists();
-
-            if (range != null) {
-                const [start, end ] = req.headers.get("Range").split("=").at(-1).split("-").map((v) => {
-                    return v ? Number(v) : Number(f.size)
-                }); // [0, 100]
-                console.log("RANGE", req.headers.get("Range"), " => ", start, end)
-                return new Response(f.slice(start, end), {
-                    headers: {
-                        "content-range": `bytes ${start}-${end}/${f.size}`,
-                        "content-type": f.type,
-                    }
-                });
-            } else if (req.method == "HEAD") {
-                return new Response(null, {
-                    headers: {
-                        "content-type": exists ? f.type : "text/plain",
-                        "content-size": exists ? f.size.toString() : "0",
-                    }
-                })
-            } else if (exists) {
-                console.log("FILE")
-                return new Response(f);
-            } else {
-                console.log("NNNN")
-                return new Response(null, {status: 404});
-            }
+            const [start, end] = parseRange(req, file);
+            return new Response(file.slice(start, end), {
+                headers: {
+                    "content-range": `bytes ${start}-${end}/${file.size}`,
+                    "content-type": file.type,
+                }
+            });
         }
     },
     // port: 3000,
