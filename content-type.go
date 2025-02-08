8c55c68a811a24e0220a383ba3523e96a9e51284
terrywh <terry.wuhao@gmail.com>
2025-02-08 21:26:45 +0800

    新增 GO 实现 (目前看 Bun 实现在客户端断连时一定概率存在异常);

---
+++ content-type.go
@@ -0,0 +1,145 @@
+package main
+
+import (
+	"fmt"
+	"path/filepath"
+	"strings"
+)
+
+func FormatSize(size int64) string {
+	if fsize := float64(size); fsize > 1.1*1024*1024*1024 {
+		return fmt.Sprintf("%.1fG", fsize/1024/1024/1024)
+	} else if fsize > 1.1*1024*1024 {
+		return fmt.Sprintf("%.1fM", fsize/1024/1024)
+	} else {
+		return fmt.Sprintf("%.1fK", fsize/1024)
+	}
+}
+
+func ParseType(path string) string {
+	switch ext := filepath.Ext(path); ext {
+	case ".mp4":
+		fallthrough
+	case ".mkv":
+		fallthrough
+	case ".webm":
+		return fmt.Sprintf("video/%s", ext[1:])
+	case ".css":
+		return "text/stylesheet"
+	case ".js":
+		return "text/javascript"
+	case ".json":
+		fallthrough
+	case ".yaml":
+		fallthrough
+	case ".toml":
+		fallthrough
+	case ".html":
+		fallthrough
+	case ".php":
+		fallthrough
+	case ".cpp":
+		fallthrough
+	case ".go":
+		fallthrough
+	case ".rs":
+		return fmt.Sprintf("text/%s", ext[1:])
+	case ".flac":
+		fallthrough
+	case ".wma":
+		fallthrough
+	case ".mp3":
+		fallthrough
+	case ".wav":
+		fallthrough
+	case ".ogg":
+		return fmt.Sprintf("audio/%s", ext[1:])
+	case ".gif":
+		fallthrough
+	case ".bmp":
+		fallthrough
+	case ".ico":
+		fallthrough
+	case ".jpg":
+		fallthrough
+	case ".jpeg":
+		fallthrough
+	case ".heic":
+		fallthrough
+	case ".webp":
+		return fmt.Sprintf("image/%s", ext[1:])
+	case ".txt":
+		return "text/plain"
+	case ".md":
+		return "text/markdown"
+	default:
+		if strings.HasSuffix(path, "/") {
+			return ""
+		} else {
+			return "application/octet-stream"
+		}
+	}
+}
+
+func ParseIcon(path string) string {
+	switch filepath.Ext(path) {
+	case ".mp4":
+		fallthrough
+	case ".mkv":
+		fallthrough
+	case ".webm":
+		return `<i class="bi bi-film"></i>`
+	case ".css":
+		fallthrough
+	case ".js":
+		fallthrough
+	case ".json":
+		fallthrough
+	case ".yaml":
+		fallthrough
+	case ".toml":
+		fallthrough
+	case ".html":
+		fallthrough
+	case ".php":
+		fallthrough
+	case ".cpp":
+		fallthrough
+	case ".go":
+		fallthrough
+	case ".rs":
+		return `<i class="bi bi-file-code"></i>`
+	case ".flac":
+		fallthrough
+	case ".wma":
+		fallthrough
+	case ".mp3":
+		fallthrough
+	case ".wav":
+		fallthrough
+	case ".ogg":
+		return `<i class="bi bi-file-music"></i>`
+	case ".gif":
+		fallthrough
+	case ".bmp":
+		fallthrough
+	case ".jpg":
+		fallthrough
+	case ".jpeg":
+		fallthrough
+	case ".heic":
+		fallthrough
+	case ".webp":
+		return `<i class="bi bi-file-image"></i>`
+	case ".txt":
+		fallthrough
+	case ".md":
+		return `<i class="bi bi-file-text"></i>`
+	default:
+		if strings.HasSuffix(path, "/") {
+			return `<i class="bi bi-folder"></i>`
+		} else {
+			return ""
+		}
+	}
+}
--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
 	"path/filepath"
 	"strconv"
@@ -31,12 +32,76 @@ func main() {
 		log.Println(r.Method, r.URL.Path)
 
 		if r.Method == "GET" && stat.IsDir() {
-			fmt.Fprint(w, "<table>\n")
-			fmt.Fprint(w, `<tr><td><a href="dde-introduction.mp4">dde-introduction.mp4</a><td><td>2025-02-08 20:23</td><td>114.8M</td>\n`)
-			fmt.Fprint(w, "</table>\n")
+
+			entries, err := os.ReadDir(path)
+			if err != nil {
+				w.WriteHeader(500)
+				return
+			}
+			fmt.Fprintf(w, `<html>
+<head>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" crossorigin="anonymous">
+</head>
+<body>
+<div class="container">
+
+<div class="row pt-3 pb-2"><div class="col-12">
+    <h5>当前路径：<code>%s</code></h5>
+</div></div>
+
+<div class="row"><div class="col-12">
+
+<table class="table">
+	<thead>
+		<tr>
+			<th>名称</th>
+            <th style="width: 14em;">时间</th>
+            <th style="width: 5em;">大小</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr><td><i class="bi bi-box-arrow-in-up"></i> <a href="../">上一层</a></td><td>&nbsp;</td><td>&nbsp;</td></tr>
+`, path)
+			for _, entry := range entries {
+				if strings.HasPrefix(entry.Name(), ".") || entry.Name() == "node_modules" {
+					continue
+				}
+				fpath := filepath.Join(path, entry.Name())
+				flink := url.PathEscape(entry.Name())
+
+				stat, err := os.Stat(fpath)
+				if err != nil {
+					continue
+				}
+
+				if stat.IsDir() {
+					flink = fmt.Sprintf("%s/", flink)
+				}
+
+				fmt.Fprint(w, "\t\t<tr>")
+				fmt.Fprintf(w, "\t\t\t<td>%s <a href=\"%s\">%s</a></td>\n", ParseIcon(flink), flink, entry.Name())
+				if stat.IsDir() {
+					fmt.Fprintf(w, "\t\t\t<td> - </td>\n")
+					fmt.Fprintf(w, "\t\t\t<td> - </td>\n")
+				} else {
+					fmt.Fprintf(w, "\t\t\t<td class=\"text-secondary\"><i class=\"bi bi-calendar2-day\"></i> %s</td>\n", stat.ModTime().Local().Format("2006-01-02 15:04"))
+					fmt.Fprintf(w, "\t\t\t<td class=\"text-secondary\">%s</td>\n", FormatSize(stat.Size()))
+				}
+				fmt.Fprint(w, "\t\t</tr>\n")
+			}
+			fmt.Fprint(w, `
+	</tbody>
+</table>
+
+</div></div>
+
+</div>
+</body>
+</html>`)
 		} else if r.Method == "HEAD" && !stat.IsDir() {
 			w.Header().Set("Content-Length", fmt.Sprint(stat.Size()))
-			w.Header().Set("Content-Type", "video/mp4")
+			w.Header().Set("Content-Type", ParseType(path))
 			w.Write(nil)
 		} else if r.Method == "GET" && !stat.IsDir() {
 			file, err := os.Open(path)
@@ -47,11 +112,9 @@ func main() {
 			defer file.Close()
 			fileRange := r.Header.Get("Range")
 
-			w.Header().Set("Content-Type", "video/mp4")
+			w.Header().Set("Content-Type", ParseType(path))
 			if len(fileRange) < 6 {
-
 				w.WriteHeader(200)
-				// _, err = io.Copy(w, io.NewSectionReader(file, 0, 256*1024))
 				_, err = io.Copy(w, file)
 				IgnoreOnError(err, 200, err)
 				return
@@ -63,9 +126,6 @@ func main() {
 			if end == 0 {
 				end = stat.Size() - 1
 			}
-
-			log.Println("ranges", start, end)
-
 			w.Header().Set("Accept-Ranges", "bytes")
 			w.Header().Set("Content-Range", fmt.Sprintf("bytes %d-%d/%d", start, end, stat.Size()))
 			w.Header().Set("Content-Length", fmt.Sprint(end-start+1))
